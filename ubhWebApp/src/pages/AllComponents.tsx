098160c77ada81c49f4d9e201d2bf0ac7c96a0ba
Jade <z5476819@ad.unsw.edu.au>
2026-05-13 23:34:56 +1000

    feat: added button component

--- ubhWebApp/src/pages/AllComponents.tsx
+++ ubhWebApp/src/pages/AllComponents.tsx
@@ -1,38 +1,46 @@
 import { Header1, Header2, Header3, Header4, Header5 } from "../components/typography/Header";
 import { Body1, Body2, Caption } from "../components/typography/Body";
 import { Input } from "../components/input/Input";
+import { Button } from "../components/buttons/Button";
+
+import { X } from 'lucide-react';   // placeholder, maybe we can move this into individual wrapper for icons.
 
 export default function AllComponents() {
   return (
     <div>
       <Header1>UBH Components Library</Header1>
+
+      {/* <-- TYPOGRAPHY SECTION --> */}
       <Header2>Typography</Header2>
-        <section>
-          <Header1>Header 1</Header1>
-          <Caption>fontSize: 40px, weight: 700, lineHeight: 1.2</Caption>
+      <section>
+        <Header1>Header 1</Header1>
+        <Caption>fontSize: 40px, weight: 700, lineHeight: 1.2</Caption>
 
-          <Header2>Header 2</Header2>
-          <Caption>fontSize: 32px, weight: 600, lineHeight: 1.3</Caption>
+        <Header2>Header 2</Header2>
+        <Caption>fontSize: 32px, weight: 600, lineHeight: 1.3</Caption>
 
-          <Header3>Header 3</Header3>
-          <Caption>fontSize: 28px, weight: 500, lineHeight: 1.35</Caption>
+        <Header3>Header 3</Header3>
+        <Caption>fontSize: 28px, weight: 500, lineHeight: 1.35</Caption>
 
-          <Header4>Header 4</Header4>
-          <Caption>fontSize: 24px, weight: 400, lineHeight: 1.4</Caption>
+        <Header4>Header 4</Header4>
+        <Caption>fontSize: 24px, weight: 400, lineHeight: 1.4</Caption>
 
-          <Header5>Header 5</Header5>
-          <Caption>fontSize: 20px, weight: 400, lineHeight: 1.45</Caption>
-        </section>
-        <section>
-          <Body1>Body 1</Body1>
-          <Caption>fontSize: 16px, weight: 400</Caption>
+        <Header5>Header 5</Header5>
+        <Caption>fontSize: 20px, weight: 400, lineHeight: 1.45</Caption>
+      </section>
+      <section>
+        <Body1>Body 1</Body1>
+        <Caption>fontSize: 16px, weight: 400</Caption>
 
-          <Body2>Body 2</Body2>
-          <Caption>fontSize: 14px, weight: 400</Caption>
+        <Body2>Body 2</Body2>
+        <Caption>fontSize: 14px, weight: 400</Caption>
 
-          <Caption>Caption / Label - 12px</Caption>
-        </section>
-      <Header4>Inputs:</Header4>
+        <Caption>Caption / Label - 12px</Caption>
+      </section>
+      {/* INPUTS */}
+      <section style={{ marginTop: "48px" }}>
+        <Header2>Inputs:</Header2>
+
         <Input variant='single-line' label='Single-line (default ⇒ hover ⇒ focused ⇒ filled)' placeholder='Enter text' />
         <Input variant='single-line' label='Single-line (error)' value='Text error' error='This field is required.'/>
         <Input variant='single-line' label='Single-line (loading)' loading={true}/>
@@ -53,6 +61,131 @@ export default function AllComponents() {
         <Input variant='phone' label='Phone (error)' value='+61 423 4' error='This field is of an invalid format.'/>
         <Input variant='phone' label='Phone (loading)' value='+61 423 456 789' loading={true}/>
         <Input variant='phone' label='Phone (disabled)' placeholder='+61 423 456 789' disabled/>
+      </section>
+      {/* BUTTONS */}
+      <section style={{ marginTop: "48px" }}>
+        <Header2>Buttons:</Header2>
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "36px"
+        }}>
+          <div style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+            <Body1>default:</Body1>
+            <Button variant='default' size='xs'>
+              x-small
+            </Button>
+
+            <Button variant='default' size='sm'>
+              small
+            </Button>
+
+            <Button variant='default' size='md'>
+              medium
+            </Button>
+
+            <Button variant='default' size='lg'>
+              large
+            </Button>
+
+            <Button variant='default' size='xl'>
+              x-large
+            </Button>
+          </div>
+
+          <div style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+            <Body1>outlined:</Body1>
+            <Button variant='outlined' size='xs'>
+              x-small
+            </Button>
+
+            <Button variant='outlined' size='sm'>
+              small
+            </Button>
+
+            <Button variant='outlined' size='md'>
+              medium
+            </Button>
+
+            <Button variant='outlined' size='lg'>
+              large
+            </Button>
+
+            <Button variant='outlined' size='xl'>
+              x-large
+            </Button>
+          </div>
+
+          <div style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+            <Body1>text:</Body1>
+            <Button variant='text' size='xs'>
+              x-small
+            </Button>
+
+            <Button variant='text' size='sm'>
+              small
+            </Button>
+
+            <Button variant='text' size='md'>
+              medium
+            </Button>
+
+            <Button variant='text' size='lg'>
+              large
+            </Button>
+
+            <Button variant='text' size='xl'>
+              x-large
+            </Button>
+          </div>
+
+          <div style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}>
+            <Button>
+              Button
+            </Button>
+
+            <Button leftIcon={X}>
+              Button
+            </Button>
+
+            <Button rightIcon={X}>
+              Button
+            </Button>
+
+            <Button leftIcon={X} rightIcon={X}>
+              Button
+            </Button>
+          </div>
+
+          <Button variant='default' disabled>
+            Disabled
+          </Button>
+
+          <Button fullWidth>
+            Full Width
+          </Button>
+        </div>
+      </section>
     </div>
   );
 }
