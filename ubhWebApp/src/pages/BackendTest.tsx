0b35b37977f57c62483660b983375e011ea6a57c
Lipda Chantayasakorn <l.chantayasakorn@student.unsw.edu.au>
2026-05-21 11:39:07 +0700

    feat: add navigation link between pages

--- ubhWebApp/src/pages/BackendTest.tsx
+++ ubhWebApp/src/pages/BackendTest.tsx
@@ -1,8 +1,21 @@
+import { Box } from '@mantine/core';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../components/buttons/Button';
+import { Header1 } from '../components/typography/Header';
+import { Body1 } from '../components/typography/Body';
+
 export default function TestPage() {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>Test Page</h1>
-      <p>This is the /test route.</p>
+      <Header1>Test Page</Header1>
+      <Body1>This is the /test route.</Body1>
+      <Box py="xl">
+        <Button variant="default" size="md" fullWidth onClick={() => navigate('/')}>
+          Back to the main page
+        </Button>
+      </Box>
     </div>
   );
 }
