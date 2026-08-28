9d35a49ea7422bb9a665344c23a5673d884b3caf
Asna Hassan <z5590539@student.unsw.edu.au>
2026-08-29 02:56:29 +1000

    style: format code with Prettier

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -1,29 +1,13 @@
 import type { SVGProps } from 'react';
-import HeartLogo from '../../assets/heartLogo.svg?react'
-import HeartLogoWithText from '../../assets/heartLogoWithText.svg?react'
+import HeartLogo from '../../assets/heartLogo.svg?react';
+import HeartLogoWithText from '../../assets/heartLogoWithText.svg?react';
 
 const logoColour = 'light-dark(var(--mantine-color-ubhRed-5), var(--mantine-color-ubhRed-9))';
 
 export function Logo(props: SVGProps<SVGSVGElement>) {
-  return (
-    <HeartLogo
-      {...props}
-      style={{
-        color: logoColour,
-        ...props.style,
-      }}
-    />
-  );
+  return <HeartLogo {...props} style={{ color: logoColour, ...props.style }} />;
 }
 
 export function FullLogo(props: SVGProps<SVGSVGElement>) {
-  return (
-    <HeartLogoWithText
-      {...props}
-      style={{
-        color: logoColour,
-        ...props.style,
-      }}
-    />
-  );
+  return <HeartLogoWithText {...props} style={{ color: logoColour, ...props.style }} />;
 }
