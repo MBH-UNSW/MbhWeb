243ed427f73ce1a3c85460c5514fdf35a006d4e4
Asna Hassan <z5590539@student.unsw.edu.au>
2026-09-12 18:12:54 +1000

    feat: added protected route so sidebar is only visible on site

---
+++ src/pages/ProtectedRoute.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+export function ProtectedRoute() {
+  const session = localStorage.getItem('session');
+  if (!session) {
+    return <Navigate to='/login' replace />;
+  }
+  return <Outlet />;
+}
