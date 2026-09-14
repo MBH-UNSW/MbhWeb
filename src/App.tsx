fcbafed89f0dca42242fe5a19c38b3ea445f1c8d
Asna Hassan <z5590539@student.unsw.edu.au>
2026-09-14 14:41:17 +1000

    feat: add basic profile page draft

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import DashboardPage from './pages/dashboard/Dashboard';
 import LoginPage from './pages/login/Login';
 import PatientsPage from './pages/Patients';
 import AppointmentsPage from './pages/Appointments';
-import ProfilePage from './pages/Profile';
+import ProfilePage from './pages/profile/Profile';
 import SettingsPage from './pages/Settings';
 import { PageTitle } from './components/pageTitle/PageTitle';
 import { ProtectedRoute } from './pages/ProtectedRoute';
